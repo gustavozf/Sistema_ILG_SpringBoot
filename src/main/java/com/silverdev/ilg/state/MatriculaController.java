fb6234921cfd548bbdf2ea1d712ea05d63e74f47
gustavozf <gzf1996@gmail.com>
2017-12-13 14:17:08 -0200

    fix bugs PP

--- src/main/java/com/silverdev/ilg/state/MatriculaController.java
+++ src/main/java/com/silverdev/ilg/state/MatriculaController.java
@@ -1,4 +1,11 @@
 package com.silverdev.ilg.state;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/stateIngressante")
 public class MatriculaController {
+
+    
 }
